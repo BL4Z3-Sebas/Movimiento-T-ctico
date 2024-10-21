547f316fb04fdc55b76d14121aadec786aa8e40c
BL4Z3-Sebas <158209851+BL4Z3-Sebas@users.noreply.github.com>
2024-10-20 19:11:04 -0500

    primer commit

---
+++ src/Modelo/Adyacencia.java
@@ -0,0 +1,38 @@
+package Modelo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Alfonso
+ */
+public enum Adyacencia {
+    adj1(new int[]{0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}),
+    adj2(new int[]{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}),
+    adj3(new int[]{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}),
+    adj4(new int[]{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0}),
+    adj5(new int[]{0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0}),
+    adj6(new int[]{0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0}),
+    adj7(new int[]{0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0}),
+    adj8(new int[]{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1}),
+    adj9(new int[]{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}),
+    adj10(new int[]{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0}),
+    adj11(new int[]{0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0});
+
+    private final int[] adyacentes;
+
+    private Adyacencia(int[] adyacentes) {
+        this.adyacentes = adyacentes;
+    }
+
+    public ArrayList getAdyacentes() {
+        return new ArrayList<>(Arrays.asList(this.adyacentes));
+    }
+
+    public static void mostrarMatriz() {
+        for (Adyacencia value : values()) {
+            System.out.println(value.getAdyacentes());
+        }
+    }
+}
---
+++ src/Modelo/Jugador.java
@@ -0,0 +1,83 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author andre
+ */
+public class Jugador {
+
+    private int velocidad;
+    private int posesion;
+    private int remate;
+    private String nombre;
+    private ArrayList<Jugador> adyacentes;
+
+    public Jugador(String nombre, int velocidad, int posesion, int remate) {
+        this.nombre = nombre;
+        this.velocidad = velocidad;
+        this.posesion = posesion;
+        this.remate = remate;
+        this.adyacentes = new ArrayList<>();
+    }
+
+    public Jugador(String nombre, int velocidad, int posesion, int remate, ArrayList adyacencia) {
+        this.nombre = nombre;
+        this.velocidad = velocidad;
+        this.posesion = posesion;
+        this.remate = remate;
+        this.adyacentes = adyacencia;
+    }
+
+    public Jugador(int velocidad, int posesion, int remate) {
+
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public int getPosesion() {
+        return posesion;
+    }
+
+    public void setPosesion(int posesion) {
+        this.posesion = posesion;
+    }
+
+    public int getRemate() {
+        return remate;
+    }
+
+    public void setRemate(int remate) {
+        this.remate = remate;
+    }
+
+    public ArrayList getJugadores() {
+        return adyacentes;
+    }
+
+    public void addJugador(Jugador jugador) {
+        this.adyacentes.add(jugador);
+        jugador.getJugadores().add(this);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + velocidad + ", " + posesion + ", " + remate;
+    }
+
+}
