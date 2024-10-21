50b9af5badb1e0253a8d2681156bc446b273b66c
adcohen1 <adcohen@uninorte.edu.co>
2024-10-20 19:20:18 -0500

    here we go again

---
+++ src/Modelo/Camino.java
@@ -0,0 +1,18 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author andre
+ */
+public class Camino {
+    ArrayList<Jugador> jugadores;
+    int peso;
+
+    public Camino() {
+        this.jugadores = new ArrayList<>();
+        this.peso = 0;
+    }
+    
+}
---
+++ src/movimiento_tactico/Movimiento_Tactico.java
@@ -0,0 +1,16 @@
+package movimiento_tactico;
+
+import Modelo.Adyacencia;
+import Modelo.Equipo;
+
+/**
+ *
+ * @author andre
+ */
+public class Movimiento_Tactico {
+
+    public static void main(String[] args) {
+        Equipo.mostrarEquipo();
+        Adyacencia.mostrarMatriz();
+    }    
+}
